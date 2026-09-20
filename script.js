092a1682a9f1ecb5c77d4fd4ccee67c56cec33da
Ashkan <ashkan.bahar@gmail.com>
2026-09-20 16:27:38 +0200

    Fixed the code, changed the comments

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+/* Note - To easily rmemeber which variable does what I've used prefixes for these variables:
+        I've used the prefix "el" for variables which hold DOM elements. 
+        I've used the prefix "new" for variables that create elements within the DOM.
+*/
+
 // Declaring the variables that hold the DOM elements.
 const elAddTaskBtn = document.querySelector("#addTaskBtn");
 const elTaskInput = document.querySelector("#taskInput");
 const elTaskList = document.querySelector("#taskList");
 const elWarningMsg = document.querySelector("#warningMsg");
+const elNumOfCompletes = document.querySelector("#numOfCompletes");
 
-// The main function.
-elAddTaskBtn.addEventListener("click", addTodoItem);
+let completedCount = 0; // Declare a variable for showing the number of completed tasks in an html placeholder, to be used in the "completed" section below.
+
+
+elAddTaskBtn.addEventListener("click", addTodoItem); // Runs the function below if the task button is clicked.
 
 function addTodoItem(){
     const inputText = elTaskInput.value; // Creates a variable from the text in the html input, to be used later in the function.
@@ -23,14 +31,19 @@ function addTodoItem(){
     newItemLabel.textContent = inputText; // sets the created span elements to be the text in the html input.
     newItem.appendChild(newItemLabel); // adds the created span element (with the input-text included) to the "li" elements which it created above.
 
-    newItemLabel.addEventListener("click", function () { // Adds the class name "completed" to items that you click on. That class name is styled in the CSS.
-        if (newItem.getAttribute("class") == "completed") { // checks to see if the completed class name is set on an item.
-            newItem.setAttribute("class", ""); // removes the class name completed from items if you click on them again.
+    // Adds the class name "completed" to list items that you click on. That class is styled in the CSS file.    
+    newItemLabel.addEventListener("click", function () { // listens to clicks on the list items, runs the function below if clicked.
+        if (newItem.getAttribute("class") == "completed") { // checks to see if class name "completed" is set on a list item.
+            newItem.setAttribute("class", ""); // removes the class name "completed" from list items if you click on them again. Which then removes the styles.
+            completedCount--; // decrement the count variable by 1; if you click on a completed list item.
         }
         else {
-            newItem.setAttribute("class", "completed"); // sets the class name completed to items you click on the first time.
+            newItem.setAttribute("class", "completed"); // sets the class name "completed" to items you click on the first time, which then puts on the styles.
+            completedCount++; // increment the count variable by 1; if you click on an uncompleted list item.
         }
-    })
+
+        elNumOfCompletes.textContent = `${completedCount} completed`; // String template to show the number of completes in the chosen html placeholder.
+    });
 
     elTaskInput.value = ""; // Clears the input after adding a task.
 }
